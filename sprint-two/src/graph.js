abf314636f6fc744c9e1af8322a1c141073838c1
Hack Reactor Student <student@hackreactor.com>
2017-08-24 15:49:39 -0700

    Implement graph, hashTable, and set

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,35 +1,57 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
+  this.vertices = {}; 
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.vertices[node] = node;
+  this.edges[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return node in this.vertices;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.vertices[node];
+  delete this.edges[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.contains(fromNode) && this.contains(toNode)) {
+    return !!(this.edges[fromNode][toNode] && this.edges[toNode][fromNode]);
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // check if both nodes are contained in the Graph
+  // if true, then add node to this.edges object for both nodes
+  if (this.contains(fromNode) && this.contains(toNode)) {
+    this.edges[fromNode][toNode] = true;
+    this.edges[toNode][fromNode] = true;
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this.contains(fromNode) && this.contains(toNode)) {
+    this.edges[fromNode][toNode] = false;
+    this.edges[toNode][fromNode] = false;
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.vertices) {
+    cb(node);
+  }
 };
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,60 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  if (!bucket) {
+    this._storage.set(index, [[k, v]]);
+  } else {
+    var updateKey = false;
+
+    bucket = bucket.map(function(keyVal) {
+      if (keyVal[0] === k) {
+        updateKey = true;
+        return [k, v];
+      } else {
+        return keyVal;
+      }
+    });
+
+
+    if (updateKey) {
+      this._storage.set(index, bucket);
+    } else {
+      bucket.push([k, v]);
+      this._storage.set(index, bucket);
+    }
+  }
+
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var val = undefined;
+
+  if (bucket) {
+    bucket.forEach(function(keyVal) {
+      if (keyVal[0] === k) {
+        val = keyVal[1];
+      }
+    });
+  }
+  return val; 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    bucket.forEach(function(keyVal) {
+      if (keyVal[0] === k) {
+        keyVal[1] = undefined;
+      }
+    });
+  }
 };
 
 
