86bcccd5a01bd4f0596b81224a8a5c4c356707d0
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:35:19 -0700

    LinkedLists and Trees

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      // assign current tail to a temp
+      // assign tail to value
+      // assign temp.next to new tail
+      var previous = list.tail;
+      list.tail = Node(value);
+      previous.next = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    //temp variable for current head.next
+    // remove current head
+    // assign list.head to the next node
+    var prevHead = list.head;
+    list.head = list.head.next;
+    if (list.head === null) {
+      list.tail = null;
+    }
+    return prevHead.value;
   };
 
   list.contains = function(target) {
+    // start at head node, check its value prop = target
+    // return true if target matches, else go to next node and repeat
+    // return false if reached end of linklist (null) and target is not found
+    var node = list.head; 
+
+    while (node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,37 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+  _.extend(newTree, treeMethods);
+  
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
 
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create a new Tree node and assign its value to the input value
+  // append the new Tree node to children prop
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  //var found = false;  
+
+  if (this.value === target) {
+    return true;
+  } 
+
+  if (this.children.length > 0) {
+    return this.children.some(function(child) {
+      return child.contains(target);
+    });
+  }
+
+  return false;
 };
 
 
