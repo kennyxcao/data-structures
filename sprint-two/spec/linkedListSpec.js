ef5a9ab98f2dbcd3e72cff0ab38c65e4cab1eaaf
Hack Reactor Student <student@hackreactor.com>
2017-08-25 16:33:33 -0700

    Doubly Linked List

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -66,3 +66,106 @@ describe('linkedList', function() {
     expect(linkedList.tail).to.equal(null);
   });
 });
+
+
+describe('doubly-linkedList', function() {
+  var linkedList;
+
+  beforeEach(function() {
+    linkedList = DoublyLinkedList();
+  });
+
+  it('should have a head and tail', function() {
+    expect(linkedList).to.have.property('head');
+    expect(linkedList).to.have.property('tail');
+  });
+
+  it('should have methods named "addToTail", "addToHead", "removeTail" "removeHead", and "contains"', function() {
+    expect(linkedList.addToTail).to.be.a('function');
+    expect(linkedList.removeHead).to.be.a('function');
+    expect(linkedList.contains).to.be.a('function');
+    expect(linkedList.addToHead).to.be.a('function');
+    expect(linkedList.removeTail).to.be.a('function');
+  });
+
+  it('should designate a new tail when new nodes are added', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.tail.value).to.equal(4);
+    linkedList.addToTail(5);
+    expect(linkedList.tail.value).to.equal(5);
+  });
+
+  it('should designate a new head when new nodes are added', function() {
+    linkedList.addToHead(5);
+    linkedList.addToHead(10);
+    expect(linkedList.head.value).to.equal(10);
+  });
+
+  it('should remove the head from the list when removeHead is called', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    expect(linkedList.head.value).to.equal(4);
+    linkedList.removeHead();
+    expect(linkedList.head.value).to.equal(5);
+  });
+
+  it('should remove the tail from the list when removeTail is called', function() {
+    linkedList.addToTail(5);
+    linkedList.addToTail(4);
+    linkedList.removeTail();
+    expect(linkedList.tail.value).to.equal(5);
+  });
+
+  it('should return the value of the former head when removeHead is called', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.removeHead()).to.equal(4);
+  });
+
+  it('should return the value of the former tail when removeTail is called', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.removeTail()).to.equal(4);
+  });
+
+  it('should contain a value that was added', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.addToHead(7);
+    linkedList.addToHead(8);
+    expect(linkedList.contains(4)).to.equal(true);
+    expect(linkedList.contains(5)).to.equal(true);
+    expect(linkedList.contains(7)).to.equal(true);
+    expect(linkedList.contains(8)).to.equal(true);
+    expect(linkedList.contains(6)).to.equal(false);
+  });
+
+  it('should not contain a value that was removed', function() {
+    linkedList.addToTail(4);
+    linkedList.addToTail(5);
+    linkedList.removeHead();
+    expect(linkedList.contains(4)).to.equal(false);
+    linkedList.removeTail();
+    expect(linkedList.contains(5)).to.equal(false);
+  });
+
+  // add more tests here to test the functionality of linkedList
+  it('should set the head and tail to the same node when the first node is added', function() {
+    linkedList.addToTail(4);
+    expect(linkedList.head.value).to.equal(4);
+    expect(linkedList.tail.value).to.equal(4);
+    linkedList.removeTail();
+    linkedList.addToHead(5);
+    expect(linkedList.head.value).to.equal(5);
+    expect(linkedList.tail.value).to.equal(5);
+  });
+
+  it('should reset the head and tail to null when the last node is removed', function() {
+    linkedList.addToTail(4);
+    linkedList.removeHead();
+    expect(linkedList.head).to.equal(null);
+    expect(linkedList.tail).to.equal(null);
+    linkedList.addToHead(5);
+    linkedList.removeTail();
+    expect(linkedList.head).to.equal(null);
+    expect(linkedList.tail).to.equal(null);
+  });
+});
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -53,6 +53,7 @@ var Node = function(value) {
 
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
@@ -63,3 +64,72 @@ var Node = function(value) {
     removeHead - O(1)
     contains - O(n)
  */
+
+var DoublyLinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    if (!list.tail) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      var lastTail = list.tail;
+      list.tail = Node(value);      
+      lastTail.next = list.tail;
+      list.tail.previous = lastTail;
+    }
+  };
+
+  list.addToHead = function(value) {
+    if (!list.head) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      var lastHead = list.head;
+      list.head = Node(value);
+      lastHead.previous = list.head;
+      list.head.next = lastHead;
+    }
+  };
+
+  list.removeHead = function() {
+    var prevHead = list.head;
+    list.head = list.head.next;
+    if (!list.head) {
+      list.tail = null;
+    } else {
+      list.head.previous = null;
+    }
+    return prevHead.value;
+  };
+
+  list.removeTail = function(value) {
+    var prevTail = list.tail;
+    list.tail = list.tail.previous;
+    if (!list.tail) {
+      list.head = null;
+    } else {
+      list.tail.next = null;
+    }
+    return prevTail.value;
+  };
+
+  list.contains = function(target) {
+    // start at head node, check its value prop = target
+    // return true if target matches, else go to next node and repeat
+    // return false if reached end of linklist (null) and target is not found
+    var node = list.head; 
+    while (node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
+  };
+
+  return list;
+};
